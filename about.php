0ebe00d46adb77b2f70c9730a3006af32e869e89
Wei Hong <chan119015@gmail.com>
2026-02-22 17:31:21 +0800

    Added about.php page from Wei Hong's work progress

---
+++ about.php
@@ -0,0 +1,9 @@
+<?php
+echo "<h1>About Our Project</h1>";
+echo "<p>This website is a collaborative project for CSEB5143. Lead by Muhammad Fauzan bin Zaidi (BSW01085344)
+
+</p>";
+echo "<p>Chan Wei Hong (BSW01084678) is responsible for this section.</p>";
+?>
+<hr>
+<a href="index.php">Back to Home</a>
