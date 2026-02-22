0c56a0384b6137242cd1a5721a923eb59982b8b7
faris isyraf <farisisyraf88@gmail.com>
2026-02-22 23:24:32 +0800

    Added member page from Faris Isyraf in progress

---
+++ member.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Member Page</title>
+</head>
+<body>
+    <h1>Team Members</h1>
+    <p>Name: Ahmad Faris Isyraf</p>
+    <p>Role: Member A</p>
+    <p>Responsibility: Develop Member Page</p>
+</body>
+</html>
